c2471aa4a2b0f1faf6ee4712e53b7615d9c019cf
Yichen <yichenlilyc@gmail.com>
2020-12-06 10:19:33 -0800

    preprocess dataset

--- processdataset.py
+++ processdataset.py
@@ -3,42 +3,38 @@ import numpy as np
 import os
 import random
 
-dirs=os.listdir("dataset/archive")
-
+dirs = os.listdir("dataset/original")
 
 #change the brightness and contrast of the image
 def imageProcess(img, lighter):
-    al = random.randint(-2,2)
+    al = random.randint(-1,1)
     alpha = al*0.1 #contrast
-    beta = random.randint(3, 10) #brightness
-    resImg = np.uint8(np.clip((alpha*img+beta*lighter), 0, 255))
+    beta = random.randint(50,100) #brightness
+    resImg = np.uint8(np.clip((img+beta*lighter), 0, 255))
     return resImg
 
-
 for dir in dirs:
-    subdir = os.path.join('dataset', 'archive', dir)
+    subdir = os.path.join('dataset', 'original', dir)
     print(subdir)
     id = 0
 
     for imgdir in os.listdir(subdir):
         print(imgdir)
         newImgDir = os.path.join(subdir, imgdir)
-        print(newImgDir)
         img = cv2.imread(newImgDir)
         img = cv2.resize(img, (224,224))
+        print("resize"+newImgDir)
         imgRotate = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-        imgLighter = imageProcess(img, 1)
+        #imgLighter = imageProcess(img, 1)
         imgDarker = imageProcess(img, -1)
 
-        cv2.imwrite(os.path.join(subdir,'generate',str(id)+".jpg"), img)
+        generatedir = os.path.join('dataset', 'archive', dir)
+        cv2.imwrite(os.path.join(generatedir, str(id)+".jpg"), img)
         id += 1
-        cv2.imwrite(os.path.join(subdir,'generate',str(id)+".jpg"), imgRotate)
+        cv2.imwrite(os.path.join(generatedir, str(id)+".jpg"), imgRotate)
         id += 1
-        cv2.imwrite(os.path.join(subdir,'generate',str(id)+".jpg"), imgLighter)
+        cv2.imwrite(os.path.join(generatedir, str(id)+".jpg"), imgDarker)
         id += 1
-        cv2.imwrite(os.path.join(subdir,'generate',str(id)+".jpg"), imgDarker)
-        id += 1
-
 
 
 
